b7b3dc82cbbe8b0dec55fbfbe55eeaf9ee24a176
Jacky Ho <jacky.s.ho@nasa.gov>
2015-04-21 17:14:47 -0700

    Changed bank_numbers and adding enter_registrar exercise.

--- bank_numbers.py
+++ bank_numbers.py
@@ -3,13 +3,11 @@ input_2 = '111111111'
 input_3 = '490067715'
 
 def bannerNumbers(acct_number):
-	for x in acct_number:
-		if x == "1":
-			current_number = " | \n |"
-			entry = entry + current_number
-		elif x == "2":
-			current_number = " _\n _|\n|_"
-			entry = entry + current_number
+	digits = [
+		["_", "| |", "|_|"], #0
+		[" ", "|", "|" ], #1
+		["_", " _|", "|_ "] #2
+	]
 
 
 	print entry
