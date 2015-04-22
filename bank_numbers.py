c36749daed003e1b9a7ee1e9936c458f6adde749
Jacky Ho <jacky.s.ho@nasa.gov>
2015-04-22 11:22:42 -0700

    Bank Numbers Pt.1 Done.

--- bank_numbers.py
+++ bank_numbers.py
@@ -1,15 +1,38 @@
+import sys
+
 input_1 = '000000000'
 input_2 = '111111111'
 input_3 = '490067715'
 
+def inputToList(nums):
+		index = []
+		for x in nums:
+			x = int(x)
+			index.append(x)
+		return index
+
 def bannerNumbers(acct_number):
-	digits = [
-		["_", "| |", "|_|"], #0
-		[" ", "|", "|" ], #1
-		["_", " _|", "|_ "] #2
-	]
+	#Digits to Reference From
+    digits = [
+        [" _ ", "| |", "|_|"]  # 0
+        , ["   ", "  |", "  |"]  # 1
+        , [" _ ", " _|", "|_ "]  # 2
+        , [" _ ", " _|", " _|"]  # 3
+        , ["   ", "|_|", "  |"]  # 4
+        , [" _ ", "|_ ", " _|"]  # 5
+        , [" _ ", "|_ ", "|_|"]  # 6
+        , [" _ ", "  |", "  |"]  # 7
+        , [" _ ", "|_|", "|_|"]  # 8
+        , [" _ ", "|_|", " _|"]  # 9
+    ]
+    numbers = inputToList(acct_number)
+    level = 0
+    depth = 3
+    while level < depth:
+    	for x in numbers:
+    		print(digits[x][level]),
+    	print("\n"),
+    	level = level + 1
 
 
-	print entry
-
-bannerNumbers("12")
+bannerNumbers("123456789")
