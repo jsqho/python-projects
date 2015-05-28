d6b5ae02f39887d26d92ebc3b44342d28c4e9ff3
Jacky Ho <jacky.s.ho@nasa.gov>
2015-05-28 12:35:28 -0700

    Fixed '?' and Space Checking

--- jacky_hangman.py
+++ jacky_hangman.py
@@ -41,9 +41,9 @@ def main():
     print "\n" * 30
 
     number_of_guesses = 0
-    number_of_wrong = 0 
+    number_of_wrong = 0
     number_of_not_equal = 0
-    q = list("?" * len(input_word)) 
+    q = list("?" * len(input_word))
     char_list = list(input_word)
     display_q = ""
     guessed_chars = ""
@@ -56,6 +56,8 @@ def main():
         char_list_enum = enumerate(list(input_word))
         if guess in guessed_chars:
             print "You already guessed the character: " + guess
+        elif guess == " " or guess == "?":
+            print "Invalid Character Guess"
         else:
             if len(guess) > 1:
                 print "You can only guess a single character."
@@ -76,14 +78,5 @@ def main():
                     print "You correctly guessed the secret word: " + "".join(q)
                     break
 
-      
+
 main()
-
-
-
-    
-    
-
-
-
-
