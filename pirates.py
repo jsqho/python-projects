9058b598067afe68c5d95d4bbdf36ef6648917dd
Jacky Ho <jacky.s.ho@nasa.gov>
2016-07-19 12:14:57 -0700

    Adding Pirates.py

--- pirates.py
+++ pirates.py
@@ -1,18 +1,22 @@
 def answer(numbers):
 	WIB = [0,]
 	work = True
+	counter = 1
 	while work == True:
-		next = [WIB[-1]]
+		next = numbers[WIB[-1]]
 		if next in WIB:
 			WIB.append(next)
-			fin = len(WIB) - 1
+			#fin = len(set(numbers))
 			work = False
-			return fin
 		else:
+			counter = counter + 1
 			WIB.append(next)
+	return counter
+	#return fin
+
 
 numbers_1 = [1,0]
-#numbers_2 = [1,2,1]
+numbers_2 = [1,2,1]
 
 print answer(numbers_1)
-#print answer(numbers_2)
+print answer(numbers_2)
